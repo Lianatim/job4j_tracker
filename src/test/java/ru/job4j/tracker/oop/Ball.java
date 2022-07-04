189812d8dfa6cc5ad5f4e77175b038e1384e6ba2
Liana <95958723+Lianatim@users.noreply.github.com>
2022-07-04 19:59:31 +0300

    Взаимодействие объектов

--- src/test/java/ru/job4j/tracker/oop/Ball.java
+++ src/test/java/ru/job4j/tracker/oop/Ball.java
@@ -1,8 +1,8 @@
 package ru.job4j.tracker.oop;
 
 public class Ball {
-    public void tryRun(boolean condition){
-        if (condition){
+    public void tryRun(boolean condition) {
+        if (condition) {
             System.out.println("Колобок съеден");
         } else {
             System.out.println("Колобок сбежал");
