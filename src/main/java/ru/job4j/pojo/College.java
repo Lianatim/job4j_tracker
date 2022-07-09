5bb9e016df2fbfbcb77f4760d8f164790215a0e4
Liana <95958723+Lianatim@users.noreply.github.com>
2022-07-09 20:02:52 +0300

    Модель данных

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,16 @@
+package ru.job4j.pojo;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Sidorov Anton");
+        student.setGroup(1123);
+        student.setDateReceipt(LocalDate.of(2013, 01, 31));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-YYYY");
+        System.out.println(("Student: " + student.getName() + ", studying in a group: "
+                + student.getGroup() + ", entered " + student.getDateReceipt().format(formatter)));
+    }
+}
