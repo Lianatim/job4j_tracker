7d72b0dc5e70ed4797ee9ff36e9b996b06d94958
Liana <95958723+Lianatim@users.noreply.github.com>
2022-07-18 18:59:56 +0300

    Тесты вывода на консоль

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -127,7 +127,7 @@ public class StartUITest {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("test1"));
-        Input in = new StubInput(new String[]{"0", String.valueOf(item.getName()), "1"});
+        Input in = new StubInput(new String[]{"0", item.getName(), "1"});
         UserAction[] actions = new UserAction[]{
                 new FindNameAction(out),
                 new ExitAction()
@@ -139,7 +139,7 @@ public class StartUITest {
                         + "0. Find items by name" + ln
                         + "1. Exit Program" + ln
                         + "=== Find items by name ===" + ln
-                        + item.toString() + ln
+                        + item + ln
                         + "Menu:" + ln
                         + "0. Find items by name" + ln
                         + "1. Exit Program" + ln
